9d854c057ba05c031a1ad1ccfa19d4c78a978a90
banjvduo <stagry@gmail.com>
2018-05-07 23:50:07 +0800

    add swagger2

--- src/main/java/com/banjvduo/demo/Swagger2.java
+++ src/main/java/com/banjvduo/demo/Swagger2.java
@@ -1,10 +1,44 @@
 package com.banjvduo.demo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * User: stagry@gmail.com
  * Date: 18/5/7
  * Time: 23:06
  * Created by IntelliJ IDEA.
  */
+
+@Configuration
+@EnableSwagger2
 public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.banjvduo.demo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger2 Test")
+                .description("这是一个 Swagger2 的测试页面")
+                .termsOfServiceUrl("https://github.com/banjvduo/springboot-demo")
+                .contact("banjvduo")
+                .version("1.0")
+                .build();
+    }
+
 }
--- src/main/java/com/banjvduo/demo/controller/UserController.java
+++ src/main/java/com/banjvduo/demo/controller/UserController.java
@@ -1,9 +1,20 @@
 package com.banjvduo.demo.controller;
 
 import com.banjvduo.demo.model.User;
-import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -11,23 +22,33 @@ public class UserController {
 
     static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
 
+    @ApiOperation(value = "获取用户列表", notes = "获取用户列表")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public List<User> getUserList() {
         List<User> r = new ArrayList<>(users.values());
         return r;
     }
 
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String postUser(@ModelAttribute User user) {
         users.put(user.getId(), user);
         return "success";
     }
 
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable Long id) {
         return users.get(id);
     }
 
+    @ApiOperation(value = "更新用户详细信息", notes = "根据url的id来更新指定对象，并根据参数user的字段信息来更新")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public String putUser(@PathVariable Long id, @ModelAttribute User user) {
         User u = users.get(id);
@@ -37,6 +58,8 @@ public class UserController {
         return "success";
     }
 
+    @ApiOperation(value = "删除用户", notes = "根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public String deleteUser(@PathVariable Long id) {
         users.remove(id);
